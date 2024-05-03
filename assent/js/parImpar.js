fe50daa92d3c70f670119975dcbf90eb969c8d59
Mariano-Rojas <marianorojas64@hotmail.com.ar>
2024-05-03 12:45:52 -0300

    Se completo la actividad par o impar

---
+++ assent/js/parImpar.js
@@ -0,0 +1,7 @@
+let numero = parseInt(prompt("Ingrese el numero que desea saber si es par o impar:"));
+
+if (numero % 2 === 0) {
+    console.log("El numero: " + numero + " es par");
+} else {
+    console.log("El numero: " + numero + " es impar");
+}
