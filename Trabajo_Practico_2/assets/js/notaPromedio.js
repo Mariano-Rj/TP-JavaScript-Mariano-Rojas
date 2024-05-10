c24caf0322812ad2e5253eb00e94de93d980f768
Mariano-Rojas <marianorojas64@hotmail.com.ar>
2024-05-10 10:28:23 -0300

    se corigieron las variables y se introdujo la secuencia if else

--- Trabajo_Practico_2/assets/js/notaPromedio.js
+++ Trabajo_Practico_2/assets/js/notaPromedio.js
@@ -1,7 +1,20 @@
-let primerTrimestre = prompt(parseInt("Ingrese la nota del primer trimestre (1-10)"));
+let primerTrimestre = parseInt(prompt("Ingrese la nota del primer trimestre (1-10)."));
 
-let segundoTrimestre = prompt(parseInt("Ingrese la nota del segundo trimestre (1-10)"));
+let segundoTrimestre = parseInt(prompt("Ingrese la nota del segundo trimestre (1-10.)"));
 
-let tercerTrimestre = prompt(parseInt("Ingrese la nota del tercer trimestre (1-10)"));
+let tercerTrimestre = parseInt(prompt("Ingrese la nota del tercer trimestre (1-10.)"));
 
 
+promedio = (primerTrimestre + segundoTrimestre + tercerTrimestre)/ 3
+
+if (promedio > 0 && promedio < 4) {
+    console.log("Nota Insuficiente");
+} else if (promedio > 3 && promedio < 6) {
+    console.log("Nota Regular");
+} else if (promedio > 5 && promedio < 8) {
+    console.log("Nota Buena");
+} else if (promedio > 7 && promedio < 10) {
+    console.log("Nota Muy Buena");
+} else if (promedio === 10) {
+    console.log("Nota Sobresaliente");
+} else {console.log("Nota Invalida, ingrese entre 1 al 10.")}
