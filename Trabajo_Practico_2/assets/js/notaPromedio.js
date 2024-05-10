7c10425404d9593c175357aa7ce4310372dc2500
Mariano-Rojas <marianorojas64@hotmail.com.ar>
2024-05-10 09:56:44 -0300

    En notaPromedio se crearon las variables necesarias para pedirle al usuario sus notas por trimestres

---
+++ Trabajo_Practico_2/assets/js/notaPromedio.js
@@ -0,0 +1,7 @@
+let primerTrimestre = prompt(parseInt("Ingrese la nota del primer trimestre (1-10)"));
+
+let segundoTrimestre = prompt(parseInt("Ingrese la nota del segundo trimestre (1-10)"));
+
+let tercerTrimestre = prompt(parseInt("Ingrese la nota del tercer trimestre (1-10)"));
+
+
